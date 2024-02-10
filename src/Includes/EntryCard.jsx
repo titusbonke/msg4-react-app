a785feb815d150c16d28c53c6005dc0f552a4213
titusbonke <titusbonkec@gmail.com>
2024-02-11 01:21:26 +0530

    .

--- src/Includes/EntryCard.jsx
+++ src/Includes/EntryCard.jsx
@@ -1,6 +1,6 @@
-function EntryCard({ children,Name,Class="card-primary" }) {
+function EntryCard({ children, Name, Class = "card-primary" }) {
     return (
-        <div className={"card "+Class} style={{ marginBottom: 0 }}>
+        <div className={"card " + Class} style={{ marginBottom: 0 }}>
             <div
                 className="card-header"
                 style={{ backgroundColor: "#007bff", color: "#fff" }}
@@ -8,13 +8,101 @@ function EntryCard({ children,Name,Class="card-primary" }) {
                 <h3 className="card-title">{Name}</h3>
             </div>
             <div className="card-body ">
-                {children.filter(a=>a.type!==EntryCardFooter).map(a=>a)}
+                {children.filter(a => a.type !== EntryCardFooter)}
             </div>
-            {children.filter(a=>a.type===EntryCardFooter).map(a=>a)}
+            {children.filter(a => a.type === EntryCardFooter)}
         </div>
     );
 }
 
+export function EntryCardWithTabs({ children, Name, Class = "card-primary" }) {
+    return (
+        <div className={`card ${Class} card-tabs`} style={{ marginBottom: 0 }}>
+            <div
+                className="card-header p-0 pt-1"
+                style={{
+                    backgroundColor: "#007bff",
+                    color: "#fff",
+                    paddingTop: ".25rem!important"
+                }}
+            >
+                <ul
+                    className="nav nav-tabs"
+                    id="cus-tab-tab"
+                    role="tablist"
+                    style={{
+                        marginBottom: "-1px",
+                        borderTopLeftRadius: ".25rem",
+                        borderTopRightRadius: ".25rem"
+                    }}
+                >
+                    <li className="pt-2 px-3">
+                        <h3 className="card-title">{Name}</h3>
+                    </li>
+                    {
+                        children.filter(a => a.type === EntryCardTab)
+                            .map(({ props: { Name, Id } }, Index) => {
+                                return (
+                                    <li className="nav-item" key={Index}>
+                                        <a
+                                            className={Index === 0 ? "nav-link active" : "nav-link"}
+                                            id={Id || Name}
+                                            data-toggle="pill"
+                                            href={"#cus-tab-" + Index}
+                                            role="tab"
+                                            aria-controls={"cus-tab-" + Index}
+                                            aria-selected="true"
+                                            title={Name}
+                                        >
+                                            {Name}
+                                        </a>
+                                    </li>
+                                )
+                            })
+                    }
+                </ul>
+            </div>
+            <div className="card-body" style={{ paddingBottom: 20 }}>
+                <div className="tab-content" id="cus-tab-tabContent">
+
+                    {
+                        children.filter(a => a.type === EntryCardTab).map(({ props: { Name, Id, children } }, Index) => {
+                            return (
+                                <>
+                                    <div
+                                        className={Index === 0 ? "tab-pane fade active show" : "tab-pane fade"}
+                                        id={"cus-tab-" + Index}
+                                        role="tabpanel"
+                                        aria-labelledby={`cus-tab-${Index}-tab`}
+                                        key={Index}
+                                    >
+                                        {children.map(a => {
+                                            if(a.type !== EntryCardFooter) return a;
+                                            else{
+                                                console.log(a)
+                                                return a.props.children;
+                                            } 
+                                        })
+                                        }
+                                    </div>
+                                </>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        </div>
+
+    );
+}
+
+
+
+export function EntryCardTab({ children, Name, Id }) {
+    return (
+        { children }
+    )
+}
 
 export function EntryCardFooter({ children }) {
     return (
--- src/Pages/AddGroup.jsx
+++ src/Pages/AddGroup.jsx
@@ -1,17 +1,39 @@
-import {TextBox,CheckBox, Button} from "../Components/Inputs";
-import EntryCard, { EntryCardFooter } from "../Includes/EntryCard";
-function AddGroup() {
+import { TextBox, CheckBox, Button } from "../Components/Inputs";
+import EntryCard, { EntryCardFooter, EntryCardTab, EntryCardWithTabs } from "../Includes/EntryCard";
+export function AddGroup() {
     return (
-        <EntryCard>
+        <EntryCard Name="Add Group" >
             <TextBox Id="GroupName" Label="Group Name" Placeholder="Group Name" Required Value={"test"} />
             <TextBox Id="GroupCode" Label="Group Code" Placeholder="Group Code" Required Value={"test"} />
-            <CheckBox Id="IsActive" Label="Is Active" Placeholder="Is Active" Required   />
+            <CheckBox Id="IsActive" Label="Is Active" Placeholder="Is Active" Required />
             <EntryCardFooter>
-                <Button Id="Close"  ClassName={"btn-danger"} Label={"Close"} OnClick={()=>window.close()} Value={"close"} />
-                <Button Id="Update"  ClassName={"btn-primary"} Label={"Update"}   />
+                <Button Id="Close" ClassName={"btn-danger"} Label={"Close"} OnClick={() => window.close()} Value={"close"} />
+                <Button Id="Update" ClassName={"btn-primary"} Label={"Update"} />
             </EntryCardFooter>
         </EntryCard>
     );
 }
 
-export default AddGroup;
+export function TestPopup() {
+    return (
+        <EntryCardWithTabs Name="Add Group" >
+            <EntryCardTab Name="Tab a" Id={"tab1"} >
+                <TextBox Id="GroupName" Label="Group Name" Placeholder="Group Name" Required Value={"test"} />
+                <TextBox Id="GroupCode" Label="Group Code" Placeholder="Group Code" Required Value={"test"} />
+                <CheckBox Id="IsActive" Label="Is Active" Placeholder="Is Active" Required />
+                <EntryCardFooter>
+                    <Button Id="Close" ClassName={"btn-danger"} Label={"Close"} OnClick={() => window.close()} Value={"close"} />
+                    <Button Id="Update" ClassName={"btn-primary"} Label={"Update"} />
+                </EntryCardFooter>
+            </EntryCardTab>
+            <EntryCardTab Name="Tab b" Id={"tab2"} >
+                <TextBox Id="GroupName" Label="Group Name" Placeholder="Group Name" Required Value={"test"} />
+                <TextBox Id="GroupCode" Label="Group Code" Placeholder="Group Code" Required Value={"test"} />
+                <CheckBox Id="IsActive" Label="Is Active" Placeholder="Is Active" Required />
+            </EntryCardTab>
+        </EntryCardWithTabs>
+
+    )
+}
+
+export default TestPopup;
